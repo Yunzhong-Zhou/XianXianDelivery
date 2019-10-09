d78f3f9282de213b3374f95646b74e2daa1568a1
YunZhong <1125213018@qq.com>
2019-10-09 11:32:33 +0800

    个人中心-首页-布局

--- app/src/main/java/com/transport/xianxian/activity/JiangLiHuoDongActivity.java
+++ app/src/main/java/com/transport/xianxian/activity/JiangLiHuoDongActivity.java
@@ -1,9 +1,11 @@
 package com.transport.xianxian.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.transport.xianxian.R;
 import com.transport.xianxian.base.BaseActivity;
+import com.transport.xianxian.utils.CommonUtil;
 
 /**
  * Created by zyz on 2019-10-01.
@@ -19,6 +21,30 @@ public class JiangLiHuoDongActivity extends BaseActivity {
     @Override
     protected void initView() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.linearLayout1:
+                //分享APP
+                /*Bundle bundle1 = new Bundle();
+                bundle1.putInt("type",1);
+                CommonUtil.gotoActivityWithData(this,JiFenLieBiaoActivity.class,bundle1,false);*/
+                break;
+            case R.id.linearLayout2:
+                //立即招募
+                /*Bundle bundle2 = new Bundle();
+                bundle2.putInt("type",2);
+                CommonUtil.gotoActivityWithData(this,JiFenLieBiaoActivity.class,bundle2,false);*/
+                break;
+            case R.id.linearLayout3:
+                //奖励记录
+                CommonUtil.gotoActivity(this, JiangLiListActivity.class, false);
+                break;
+
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/transport/xianxian/model/JiangLiListModel.java
@@ -0,0 +1,9 @@
+package com.transport.xianxian.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by zyz on 2019-10-09.
+ */
+public class JiangLiListModel implements Serializable {
+}
